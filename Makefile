58817e69c5cd8bf64f1235ff6dfbd3cfbc589454
Sasha <samsiki50@gmail.com>
2025-08-26 23:35:02 +0300

    add proto

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: proto
+proto: ## Регенерация протоколов pb
+	if not exist api\pb mkdir api\pb
+	protoc --go_out=api/pb --go-grpc_out=api/pb --proto_path=api/proto api/proto/products.proto
---
+++ schema/migrations/001_init.up.sql
@@ -0,0 +1,34 @@
+CREATE TYPE gender AS ENUM(
+    'F',
+    'M',
+    'U'
+);
+
+CREATE TYPE season AS ENUM (
+    'winter',
+    'spring',
+    'summer',
+    'autumn'
+);
+
+
+CREATE TABLE product(
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    price BIGINT NOT NULL,
+    description TEXT,
+    gender gender NOT NULL,
+    season season,
+    sizes JSONB NOT NULL,
+    is_active BOOL NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE images(
+    id SERIAL PRIMARY KEY,
+    id_product INT REFERENCES product (id),
+    url TEXT NOT NULL,
+    is_main BOOL
+)
